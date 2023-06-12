fc95c5013366ccb5b313054759882ae7e327060f
ryan-swanson <rswanson95@gmail.com>
2023-06-11 17:52:00 -0700

    include contact and note models

--- apps/chatterbox_app/admin.py
+++ apps/chatterbox_app/admin.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Contact, Note
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -15,3 +15,5 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Contact)
+admin.site.register(Note)
