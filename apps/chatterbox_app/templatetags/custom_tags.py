934f4bc8a69211b4300a70b9fab452d7530a76d3
ryan-swanson <rswanson95@gmail.com>
2023-06-14 18:28:42 -0700

    Add custom tag to display utf-8

--- apps/chatterbox_app/templatetags/custom_tags.py
+++ apps/chatterbox_app/templatetags/custom_tags.py
@@ -1,7 +1,20 @@
 from django import template
+from email.header import decode_header
 
 register = template.Library()
+
 
 @register.filter(name='proper_case')
 def proper_case(value):
     return value.title()
+
+
+@register.filter(name='decode_subject')
+def decode_subject(subject):
+    decoded_subject_parts = []
+    for text, encoding in decode_header(subject):
+        if isinstance(text, bytes):
+            decoded_subject_parts.append(text.decode(encoding or 'utf-8'))
+        else:
+            decoded_subject_parts.append(text)
+    return ' '.join(decoded_subject_parts)
