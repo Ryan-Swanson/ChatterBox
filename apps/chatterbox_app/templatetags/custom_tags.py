69859f9a71ab314d68664ebd36dd25ea71db1ed5
ryan-swanson <rswanson95@gmail.com>
2023-06-21 12:48:02 -0700

    single email display implemented

--- apps/chatterbox_app/templatetags/custom_tags.py
+++ apps/chatterbox_app/templatetags/custom_tags.py
@@ -10,13 +10,24 @@ def proper_case(value):
 
 @register.filter(name='decode_subject')
 def decode_subject(subject):
+    if subject is None:
+        return ''
+
     decoded_subject_parts = []
     for text, encoding in decode_header(subject):
         if isinstance(text, bytes):
             try:
-                decoded_subject_parts.append(text.decode(encoding or 'utf-8', errors='replace'))
+                decoded_subject_parts.append(text.decode(encoding or 'utf8', errors='replace'))
             except (UnicodeDecodeError, LookupError):
-                decoded_subject_parts.append(text.decode('utf-8', errors='replace'))
+                decoded_subject_parts.append(text.decode('utf8', errors='replace'))
         else:
             decoded_subject_parts.append(text)
     return ' '.join(decoded_subject_parts)
+
+@register.filter(name='test')
+def test(value):
+    return value['Message-ID']
+
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    return dictionary.get(key)
